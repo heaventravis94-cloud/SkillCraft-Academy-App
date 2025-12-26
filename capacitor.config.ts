74cb1ab848d3f371bbdead56e41ae55cb4f161dd
Heaven Travis <appmanagement.ht@gmail.com>
2025-12-25 19:47:23 -0500

    Initial commit from Dyad

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.skillcraftacademyapp.app',
+  appName: 'SkillCraft Academy',
+  webDir: 'dist'
+};
+
+export default config;
